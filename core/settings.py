f01310e061fbff1ef2c5343cae769d475fa28cb0
ntaliarojas <llerlynatisrojaspuertas@gmail.com>
2024-03-28 01:33:11 -0500

    urll frontend

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    #"https://payroll-front.onrender.com",
+    "https://front-payroll-gt-nr.onrender.com",
 ]
 
 # CORS_ORIGIN_WHITELIST = [
